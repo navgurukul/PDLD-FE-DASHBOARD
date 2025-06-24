bdb0f7395e466f75f5a2869aab97a7bf576a31db
Chhaya-Verma <chhayav300@gmail.com>
2025-06-24 09:25:22 +0530

    fix the hide icon size

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -89,7 +89,7 @@ const Sidebar = () => {
           </div>
         ) : (
           <div className="flex items-center pl-1">
-            <img src={CloseSidebarIcon} alt="Show" className="w-4 h-4 mr-4" />
+            <img src={CloseSidebarIcon} alt="Show" className="w-5 h-5 mr-4" />
             <span> </span>
           </div>
         )}
